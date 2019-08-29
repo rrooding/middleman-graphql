c413fe79ffd783ee31dfff0393b1d1274e5d6dc5
Ralph Rooding <ralph@ralphrooding.com>
2019-08-29 09:49:33 +0200

    Added error handling and enable flag

--- lib/middleman-graphql/commands/graphql.rb
+++ lib/middleman-graphql/commands/graphql.rb
@@ -9,13 +9,18 @@ module Middleman
       check_unknown_options!
 
       def graphql
+        unless extension.options[:enabled]
+          puts "== GraphQL queries disabled in config.rb (graphql.enabled = false)"
+          exit
+        end
+
         raise Thor::Error.new "You need to activate the graphql extension in config.rb before you can import data" if extension.nil?
 
         query_files.each do |filename|
           query(filename)
         end
 
-        logger.info 'GraphQL import: Done'
+        puts "== GraphQL import: Done"
       end
 
       def self.exit_on_failure?
@@ -37,14 +42,23 @@ module Middleman
       end
 
       def query(filename)
+        print "== Running query #{File.basename(filename)}"
         request = File.read(filename)
         result = client.query(request)
         raise "No data retreived" unless result.data
         write_result(File.basename(filename, '.graphql'), result)
+      rescue Graphlient::Errors::ServerError, NoMethodError => e
+        # We need to catch NoMethodError here because of a bug in Graphlient
+        # url: https://github.com/ashkan18/graphlient/issues/51
+
+        puts "\n== Error connecting to the API (#{extension.options[:endpoint]}): #{e.message}"
+        exit 1
       end
 
       def write_result(target, data)
-        File.open(app.root_path.join('data', target + '.yml'), 'w') do |f|
+        filename = target + '.yml'
+        puts "== => #{filename}"
+        File.open(app.root_path.join('data', filename), 'w') do |f|
           f.write data.original_hash['data'].to_yaml
         end
       end
@@ -65,10 +79,6 @@ module Middleman
 
       def extension
         @extension = app.extensions[:graphql]
-      end
-
-      def logger
-        ::Middleman::Logger.singleton
       end
 
       Base.register(self, 'graphql', 'graphql', 'Import GraphQL data to your Data folder')
--- lib/middleman-graphql/extension.rb
+++ lib/middleman-graphql/extension.rb
@@ -2,6 +2,7 @@ require "middleman-graphql/version"
 
 module MiddlemanGraphql
   class Extension < ::Middleman::Extension
+    option :enabled, false, 'Should we run queries or not'
     option :endpoint, nil, 'The GraphQL endpoint to use'
     option :token, nil, 'The authentication token to use'
   end
